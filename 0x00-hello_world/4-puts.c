2f3e53efd2e30bfa2011f83c8054453a1b967014
Shay-Lassie <lassie034@gmail.com>
2022-04-21 06:27:56 -0700

    new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@ int main(void)
  *The function main uses puts to print to standrd output
  */
 {
-	char *str1 = "\"Programming is like building a multilingual puzzle";
+	char *str1 = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(const char *str1);
 	return (0);
