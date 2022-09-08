fdb2910de6ae198161093544328c61a64d4e2f44
Shay-Lassie <lassie034@gmail.com>
2022-09-08 19:35:51 +0000

    print comments adjusted

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /*
- * To print using the printf function
+ * main - To print using the printf function
+ *
+ * Description: Adjusting description commenting style
+ *
+ * Return: Always 0 (Succeed)
  */
 
 int main(void)
