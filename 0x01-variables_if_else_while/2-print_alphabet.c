d6f7845d8d07faf51761a5819bd96bda17eb10e0
Shay-Lassie <lassie034@gmail.com>
2022-09-15 16:29:07 +0000

    print the alphabet in LC

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- print the alphabet in lowercase
+ * Description: loop through the alphabet and print in LC
+ * Return: 0
+ */
+
+int main (void){
+	char a;
+	for (a = 'a'; a <= 'z'; ++a)
+		putchar(a);
+	return(0);
+}
