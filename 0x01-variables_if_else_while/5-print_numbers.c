514360a24e86e3a69c79287b3db504f74d28e253
Shay-Lassie <lassie034@gmail.com>
2022-09-15 17:59:13 +0000

    print base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - prints base 10 numbers
+ * Description: print 0 to 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; ++i)
+		printf(i, "\n");
+
+	return (0);
+}
