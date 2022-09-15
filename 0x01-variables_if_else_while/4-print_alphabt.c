994d06c0ec80c6c66edfe0dd6a32e92bd627dc24
Shay-Lassie <lassie034@gmail.com>
2022-09-15 17:43:47 +0000

    keep pushing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print in lowercase
+ * Description: except q and e
+ * Return 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+		if (a != 'e' && a != 'q')
+			putchar (a);
+	putchar ('\n');
+
+	return (0);
+}
