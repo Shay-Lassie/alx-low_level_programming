093886582bde52b7e54cce23a6258c87f6bee45e
Shay-Lassie <lassie034@gmail.com>
2022-09-15 11:10:53 +0000

    Commenting Style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,14 @@
 #include <time.h>
 /* This program will assign a random number to n each time it is executed */
 
-/** main: Complete the source code
+/** 
+ * main - Complete the source code
  *
  * Description: Print positve negative or zero
  *
  * Return: 0
  *
- * */
+ */
 int main(void)
 {
 	int n;
