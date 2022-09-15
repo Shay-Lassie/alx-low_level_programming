6013368b4e7624c9b82b80186fec51a0f78871f5
Shay-Lassie <lassie034@gmail.com>
2022-09-15 11:48:38 +0000

    put back the curly braces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,9 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-if (n < 0) 
-{	
+	if (n < 0) {	
 	printf("%d is negative\n", n);
-}
+	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
@@ -29,9 +28,8 @@ if (n < 0)
 	{
 		printf("%d is positive\n", n);
 	}
-else
-{
+	else{
 	printf("Is not a number\n");
-}
-return (0);
+	}
+	return (0);
 }
