2c2a0bfaba669fbd6bd445b24fcd94d0ecff7d16
Shay-Lassie <lassie034@gmail.com>
2022-09-16 16:05:40 +0000

    Ndanga ndanyanya the indentation on the last for loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,15 +14,15 @@ int main(void)
 	for (x = 0; x < 9; x++)
 	{
 		for (y = x + 1; y < 10; y++)
-			{
-				putchar((x % 10) + '0');
-				putchar((y % 10) + '0');
-				if (x == 8 && y == 9)
-					continue;
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+			if (x == 8 && y == 9)
+				continue;
 
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
