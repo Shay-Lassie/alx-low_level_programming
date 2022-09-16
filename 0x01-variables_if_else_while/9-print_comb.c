402aed5e29df6a3aa5ad21ccb8000857e676b0a3
Shay-Lassie <lassie034@gmail.com>
2022-09-16 15:09:22 +0000

    4 putchars

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all single-digit number
+ * Description: only use putchar 4 times
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 9;
+
+	for (x = 0; x < 9; ++x)
+	{
+		putchar ((x % 10) + '0');
+		putchar (',');
+		putchar (' ');
+	}
+ 	putchar ((x % 10) + '0');
+
+	return (0);
+}
