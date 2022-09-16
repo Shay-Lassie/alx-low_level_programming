fa9f3fe8eaf302f48a48c30d2660a54e2b1764cc
Shay-Lassie <lassie034@gmail.com>
2022-09-16 16:43:25 +0000

    it has all been worth the headaches

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - two 2 digit number combinations
+ * Description: putchar eight times
+ * Return: 0
+ */
+
+int main (void)
+{
+	int x = 0;
+	int y = 0;
+
+	for (x = 0; x <= 98; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+			putchar ((x / 10) + '0');
+			putchar ((x % 10) + '0');
+			putchar (' ');
+			putchar ((y / 10) + '0');
+			putchar ((y % 10) + '0');
+			if (x == 98 && y == 99)
+				continue;
+			putchar (',');
+			putchar (' ');
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}
