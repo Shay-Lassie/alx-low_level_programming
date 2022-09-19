226116b9ce14c8f99bc1f6bc37e2d699e7eb0a43
Shay-Lassie <lassie034@gmail.com>
2022-09-19 08:03:30 +0000

    added main.h and defined the function to print in there:

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - print _putchar
  * Description: print a string plus newline
  * Return: 0
  */
-
 int main(void)
 {
-	printf("_putchar\n");
+	print_put();
 
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print_put()
+{
+	printf ("_putchar\n");
+}
