02212f926db8bd017f76c7e9b9a741ae771a5644
Shay-Lassie <lassie034@gmail.com>
2022-09-20 16:57:50 +0000

    added the description in description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,11 @@
 /**
  * reset_to_98 - take the reset prototype
  * Description: manipulate value of n through the pointer
+ * @n: n you have given me headaches
  * Return: Always 0
  */
 
-void reset_to_98(int *n)	
-/* @n: assign value using pointer */
+void reset_to_98(int *n)	/* @n: assign value using pointer */
 {
 	*n = 98;	/* @n: change value to 98 */
 }
