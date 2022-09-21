4cdbdb1155e8ef16a267bd9e7bae249bad021279
Shay-Lassie <lassie034@gmail.com>
2022-09-21 17:56:32 +0000

    added at the start

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,11 @@
 /**
  * _putchar - print lowercase alphabet
  * Description: putchar twice
+ * @print_alphabet: function definition
  * Return: 0
  */
 
-void print_alphabet(void) 
-/* @print_alphabet: function definition */
+void print_alphabet(void) /* @print_alphabet: function definition */
 { /* tried to describe in main.h butr no changes */
 	char a;
 
