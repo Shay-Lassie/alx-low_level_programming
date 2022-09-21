212567579055b45b60e8db489f26bf80c7bec059
Shay-Lassie <lassie034@gmail.com>
2022-09-21 18:02:50 +0000

    error of ommission honestly, thought l had fixed that part

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _putchar - print lowercase alphabet
+ * print_alphabet - print lowercase alphabet
  * Description: putchar twice
- * @print_alphabet: function definition
  * Return: 0
  */
 
 void print_alphabet(void) /* @print_alphabet: function definition */
-{ /* tried to describe in main.h butr no changes */
+{
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
