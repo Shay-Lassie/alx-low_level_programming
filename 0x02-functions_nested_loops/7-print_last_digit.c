12242e3cd18fc1e5d4d48c5f178d378f95d1e49d
Shay-Lassie <lassie034@gmail.com>
2022-09-22 08:02:49 +0000

    dont know how to take an int without a parameter and then calculate it when it is not in a variable

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * Description: takes an integer and prints the last number
+ * @int: number
+ * Return: int
+ */
+
+int print_last_digit(int)
+{
+	int r;
+
+	r = r % 10;
+
+	return (r);
+}
