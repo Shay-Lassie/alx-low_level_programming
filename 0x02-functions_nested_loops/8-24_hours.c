059b219d7bd78f1e64fd68c01a976c548d219e31
Shay-Lassie <lassie034@gmail.com>
2022-09-22 15:37:18 +0000

    for each hour, print every minute

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,25 @@
 #include "main.h"
 
 /**
- *
+ * jack_bauer - prints every minute in a day
+ * Description: 00:00 to 23:59
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}		
+}
