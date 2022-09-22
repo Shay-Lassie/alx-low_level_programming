5fc1d9ed94444103dcfe6aca4606276cac0abf23
Shay-Lassie <lassie034@gmail.com>
2022-09-22 06:44:15 +0000

    lowercase and UC character identification, also added a gitignore file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
  * Description: use char a as parameter
  * Return: Always 0
  */
+
 int _putchar(char c);
 
 void print_put(void);
@@ -14,3 +15,5 @@ void print_alphabet(void); /*print lowercase alphabet */
 void print_alphabet_x10(void);
 
 int _islower(int c);
+
+int _isalpha(int c);
