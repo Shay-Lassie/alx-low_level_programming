a601e3b6aa4d616452b9e5289755ce2259deecc0
Shay-Lassie <lassie034@gmail.com>
2022-09-26 16:13:03 +0000

    return space

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/** *_memset - fill memory with a constant byte
+/**
+ * _memset - fill memory with a constant byte
  * Description: fill the first n bytes of memory area pointed to by s
- * with constant byte b i.e b is the variable, s address
  * @s: address of variable b of char type
  * @b: char type variable
  * @n: specific number of bytes to fill n
@@ -19,6 +19,6 @@ char *_memset(char *s, char b, unsigned int n)
 		*s++ = b;
 		i++;
 	}
-	
+
 	return (pointer);
 }
