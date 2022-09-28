38255cd97ac8080a26329b3ca95c91775f7069ef
Shay-Lassie <lassie034@gmail.com>
2022-09-28 16:06:46 +0000

    didnt betty, my bad

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately
 */
- int palind(char *a, int l)
+int palind(char *a, int l)
 {
 	if (*a == 0)
 		return (l - 1);
