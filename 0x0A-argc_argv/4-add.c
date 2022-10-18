e18e02261e86b5514524561faf9f2699a93395c8
Shay-Lassie <lassie034@gmail.com>
2022-10-18 14:36:30 +0000

    hoe this fixes the bug

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,14 +11,15 @@
 int main(int argc, char **argv)
 {
 	int i = 1;
+	int j = 0;
 	int ans = 0;
 
 	if (argc < 1)
 		return (0);
 
-	for (i = 1; i < argc; i++)
+	for (j = 0; argv[i][j] != '\0'; j++)
 	{
-		if (!atoi(argv[i]))
+		if (argv[i][j] < 47 || argv[i][j] > 57)
 		{
 			printf("%s\n", "Error");
 			return (1);
