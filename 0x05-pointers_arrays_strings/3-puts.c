cedbe9d654121feb6108fd013456498045853f03
Shay-Lassie <lassie034@gmail.com>
2022-10-18 10:27:40 +0000

    moved directory'

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print string followed by a newline
+ * Description: performs the same function as puts
+ * @str: the string
+ * Return: str
+ */
+
+void _puts(char *str)
+{
+	printf("%d\n", *str);
+
+	return (str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -12,3 +12,5 @@ void reset_to_98(int *n);	/* function declaration */
 void swap_int(int *a, int *b);	/* swap integer values */
 
 int _strlen(char *s);	/* string length */
+
+void _puts(char *str);
