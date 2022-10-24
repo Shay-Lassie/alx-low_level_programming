e0995e257a085782e6760e7982ffb0f014c8ea28
Shay-Lassie <lassie034@gmail.com>
2022-10-24 15:26:38 +0000

    amazingly did this, small progress lm happy

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print 1 ti 100 the fizz buzz
+ * Description: x3 - Fizz x5 - Buzz both - FizzBuzz
+ * Return: print nimbers
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if ((x % 3) != 0 && (x % 5) != 0)
+		{
+			printf("%d ", x);
+		}
+		else if ((x % 3) == 0 && (x % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((x % 3) == 0)
+			printf("Fizz ");
+		else if ((x % 5) == 0)
+			printf("Buzz ");
+	}
+
+	return (0);
+}
