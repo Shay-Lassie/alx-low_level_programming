b6d57312340037aeefbeb9980428ebcb70056570
Shay-Lassie <lassie034@gmail.com>
2022-11-05 11:02:07 +0000

    append text only to existing files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,34 +1,36 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - appends text to the end of a file
+ * Description: if file doesnt exist, dont create
+ * @filename: file to append
+ * @text_content: text to append
+ * Return: 1 or -1
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-		int file, wr, i = 0;
+	int file;
+	ssize_t length = 0;
+	ssize_t len = 0;
+	char *ptr;
 
-			if (filename == NULL)
-						return (-1);
+	if (filename == NULL)
+		return (-1);
 
-				file = open(filename, O_RDWR | O_APPEND);
-					if (file == -1)
-								return (-1);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0, ptr = text_content; *ptr; ptr++)
+			len++;
+		length = write(file, text_content, len);
+	}
 
+	if (close(file) == -1 || len != length)
+		return (-1);
 
-						while (text_content[i])
-									i++;
-
-							if (text_content == NULL)
-									{
-												close(file);
-														return (1);
-															}
-								else
-										{
-													wr = write(file, text_content, i);
-														}
-
-									if (wr == -1)
-												return (-1);
-
-										close(file);
-											return (1);
+	return (1);
 }
