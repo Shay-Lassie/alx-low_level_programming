6f1559bc4a0b73abca4d39e428c48662988dcca8
Shay-Lassie <lassie034@gmail.com>
2022-11-30 11:40:28 +0000

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct-dog - Drogo my dog
+ * @name: Drogo
+ * @age: 5 months
+ * @owner: landlord
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
